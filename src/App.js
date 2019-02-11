f8917ac76f4065efe8e31ec399ed586aa8409189
Ross <Ross@myyearofcode.com>
2019-02-11 16:43:01 -0500

    pulling data object and print random q/a pair to console - no gui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
 class App extends Component {
   render() {
+    let thisLanguage = data.javascript
+    // console.dir(data.javascript)
+    // console.log(data[0].javascript)
+    // console.log(typeof(data))
+    // data.forEach(language => {
+      // if (language === 'javascript'){
+        // console.log(language)
+      // }
+    // })
+    console.log(thisLanguage)
+    let thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
+    let [thisQuestion, thisAnswer] =  thisQuestionString.split(":")
+    console.log(thisQuestion)
+    console.log(thisAnswer)
     return (
       <div className="App">
         <header className="App-header">
