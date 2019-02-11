1ccd8f15796e2245f515176a2fb2ebdd9fa44bba
Ross <Ross@myyearofcode.com>
2019-02-11 16:53:14 -0500

    display question in html, no answer.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import data from './data';
 
 class App extends Component {
   render() {
     let thisLanguage = data.javascript
-    // console.dir(data.javascript)
-    // console.log(data[0].javascript)
-    // console.log(typeof(data))
-    // data.forEach(language => {
-      // if (language === 'javascript'){
-        // console.log(language)
-      // }
-    // })
-    console.log(thisLanguage)
+    // console.log(thisLanguage)
     let thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
     let [thisQuestion, thisAnswer] =  thisQuestionString.split(":")
     console.log(thisQuestion)
@@ -22,18 +13,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {thisQuestion}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
