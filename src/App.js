974390b0108d40e2ef3a1db8ef5aafb3540cdcfb
Ross <Ross@myyearofcode.com>
2019-02-11 23:39:39 -0500

    adding css and it's working! Text field is next.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import data from './data';
 
+let questionCounter = 0;
 class App extends Component {
   render() {
     let thisLanguage = data.javascript
     let thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
     let [thisQuestion, thisAnswer] = thisQuestionString.split(":")
-    console.log(thisQuestion)
-    console.log(thisAnswer)
-    let questionCounter = 0;
+
     document.addEventListener ("keydown", function (zEvent) {
       if (zEvent.metaKey  &&  zEvent.code === "Enter") {
         // DO STUFF HERE
@@ -20,8 +19,11 @@ class App extends Component {
           document.getElementById('answer').classList.remove('hidden')
         }
         else {
-          thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
-          [thisQuestion, thisAnswer] = thisQuestionString.split(":")
+          thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)];
+          document.getElementById('question').innerHTML = thisQuestionString.split(":")[0]
+          document.getElementById('answer').classList.add('hidden')
+          document.getElementById('answer').classList.remove('visible')
+          document.getElementById('answer').innerHTML = thisQuestionString.split(":")[1]
         }
       }
     } );
@@ -29,11 +31,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            <div id="question">
-              {thisQuestion}
-            </div>
-            <div id="answer" className="hidden">
-              {thisAnswer}
+            <div id="outerContainer">
+              <div id="stats" className="secondary_text">
+                {Math.floor(questionCounter/2.0)} cards
+              </div>
+              <div id="qaWrapper">
+                <div id="question">
+                {thisQuestion}
+                </div>
+                <div id="answer" className="hidden">
+                  {thisAnswer}
+                </div>
+              </div>
+              <div id="instructions" className="secondary_text">
+                command-enter to advance
+              </div>
             </div>
           </p>
         </header>
