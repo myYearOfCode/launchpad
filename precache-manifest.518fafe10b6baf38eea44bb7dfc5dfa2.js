35eb7590eb346cb57858bee0eef7e2a16af35e82
Ross <Ross@myyearofcode.com>
2019-02-11 22:24:04 -0500

    Updates

---
+++ precache-manifest.518fafe10b6baf38eea44bb7dfc5dfa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a340fc0d735db7a8db7d",
+    "url": "/launchpad/static/js/runtime~main.a340fc0d.js"
+  },
+  {
+    "revision": "e6261a2e17efc3dec7ef",
+    "url": "/launchpad/static/js/main.e6261a2e.chunk.js"
+  },
+  {
+    "revision": "b4703f8943fc92441767",
+    "url": "/launchpad/static/js/2.b4703f89.chunk.js"
+  },
+  {
+    "revision": "e6261a2e17efc3dec7ef",
+    "url": "/launchpad/static/css/main.7bac48bf.chunk.css"
+  },
+  {
+    "revision": "8eca0ece7fa08dc034580b1e35533ceb",
+    "url": "/launchpad/index.html"
+  }
+];
