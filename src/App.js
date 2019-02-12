26f1eac48cd348ac34be77a7e06291ac644a7b48
Ross <Ross@myyearofcode.com>
2019-02-11 23:09:14 -0500

    setup github pages and fading of answer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import data from './data';
 class App extends Component {
   render() {
     let thisLanguage = data.javascript
-    // console.log(thisLanguage)
     let thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
-    let [thisQuestion, thisAnswer] =  thisQuestionString.split(":")
+    let [thisQuestion, thisAnswer] = thisQuestionString.split(":")
     console.log(thisQuestion)
     console.log(thisAnswer)
+    let questionCounter = 0;
+    document.addEventListener ("keydown", function (zEvent) {
+      if (zEvent.metaKey  &&  zEvent.code === "Enter") {
+        // DO STUFF HERE
+        questionCounter += 1;
+        console.log(questionCounter, (questionCounter % 2 !== 0))
+        if (questionCounter % 2 !== 0){
+          document.getElementById('answer').classList.add('visible')
+          document.getElementById('answer').classList.remove('hidden')
+        }
+        else {
+          thisQuestionString = thisLanguage[Math.floor(Math.random() * thisLanguage.length)]
+          [thisQuestion, thisAnswer] = thisQuestionString.split(":")
+        }
+      }
+    } );
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            {thisQuestion}
+            <div id="question">
+              {thisQuestion}
+            </div>
+            <div id="answer" className="hidden">
+              {thisAnswer}
+            </div>
           </p>
         </header>
       </div>
