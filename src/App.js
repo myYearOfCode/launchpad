fe999bf612fb3b5ebb85e8ba3a5830bf86c5d361
Ross <Ross@myyearofcode.com>
2019-02-16 22:47:57 -0500

    new data source. add clear text box after enter. randomize questions. handle end of question list.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import data from './data';
+import * as data2 from './data2.json';
 import LanguageMenu from './components/LanguageMenu';
 
 // TODO:
 /*
 keep track of questions to prevent duplicates from showing up.
+wipe text box on submission
+ruby .index is actually .find_index
+make text field size bigger
 */
-// code below converted array data to json data.
-// let myObject = {}
-// for (let language in data) {
-//   myObject[language] = {}
-//   console.log(language)
-//   data[language].forEach((question, index) => {
-//     console.log(`************ ${question} **************`)
-//     myObject[language][index] = {'question' : question.split("~")[0], 'answer' : question.split("~")[1]}
-//   })
-// }
-// // console.dir(myObject)
-// console.log(JSON.stringify(myObject))
+
+
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       numCards: 0,
-      language: 'ruby'
+      language: 'javascript'
     }
   }
 
@@ -49,6 +43,62 @@ class App extends Component {
     this.updateCounters();
   }
 
+  getNewQuestionLocally = () => {
+    try {
+    document.getElementsByClassName('textarea')[0].value = "" // reset text box
+    }
+    catch {
+  
+    }
+    let questionsRemaining = JSON.parse(window.localStorage.getItem('questionsRemaining'));
+    let question;
+    let answer;
+    if (questionsRemaining.length > 0) {
+      let currentQuestion = data2.javascript[questionsRemaining.pop()]
+      window.localStorage.setItem('questionsRemaining', JSON.stringify(questionsRemaining));
+      question = currentQuestion['question']
+      answer = currentQuestion['answer']
+    } else if (questionsRemaining.length === 0) {
+      question = "You have completed the set of cards! Play again?"
+      answer = "The answer to this question is always yes."
+    } else {
+      question = "Something weird happened"
+      answer = "Hold on."
+    }
+    this.setState({
+      currentQuestion: question,
+      currentAnswer: answer,
+    })
+    this.updateCounters();
+  }
+
+  /**
+   * Randomly shuffle an array
+   * https://stackoverflow.com/a/2450976/1293256
+   * @param  {Array} array The array to shuffle
+   * @return {String}      The first item in the shuffled array
+   */
+  shuffle = (array) => {
+
+  	var currentIndex = array.length;
+  	var temporaryValue, randomIndex;
+
+  	// While there remain elements to shuffle...
+  	while (0 !== currentIndex) {
+  		// Pick a remaining element...
+  		randomIndex = Math.floor(Math.random() * currentIndex);
+  		currentIndex -= 1;
+
+  		// And swap it with the current element.
+  		temporaryValue = array[currentIndex];
+  		array[currentIndex] = array[randomIndex];
+  		array[randomIndex] = temporaryValue;
+  	}
+
+  	return array;
+
+  };
+
   updateCounters = () => {
    let counter = JSON.parse(window.localStorage.getItem('total_count'));
    window.localStorage.setItem('total_count', JSON.stringify(counter + 1));
@@ -57,19 +107,21 @@ class App extends Component {
   }
 
   componentWillMount = () => {
-    //reset session_counter
+    // reset session_counter
     window.localStorage.setItem('session_counter', JSON.stringify(0));
-    // let language = data.[language] // eventually have a picker
-    this.getNewQuestion(data[this.state.language]) // for first run only
+    // console.log(this.state.language)
+    // let currentLanguage = this.state.language
+    // console.log(Object.keys(data2[currentLanguage]))
+    // window.localStorage.setItem('questionQueue', JSON.stringify(data2.javascript));
+    window.localStorage.setItem('questionsRemaining', JSON.stringify(this.shuffle(Object.keys(data2.javascript))));
+    this.getNewQuestionLocally() // for first run only
     document.addEventListener ("keydown",  (zEvent) => {
-      // console.log(zEvent.keyCode)
       if (zEvent.metaKey  &&  zEvent.keyCode === 13) {
         this.hideAnswer()
-        setTimeout(this.getNewQuestion(data[this.state.language]),100)
+        setTimeout(this.getNewQuestionLocally(),100)
         this.setState({ numCards: this.state.numCards + 1})
       }
       if (zEvent.metaKey  &&  zEvent.keyCode === 191) {
-        // questionCounter += 1;
         setTimeout(this.showAnswer, 100)
       }
       // console.log(this.state)
@@ -81,14 +133,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <LanguageMenu /> */}
+
             <div id="outerContainer">
+              <div id = "language" className="secondary_text">
+                <h1>
+                  {this.state.language}
+                </h1>
+              </div>
+              <LanguageMenu languages={Object.keys(data)}/>
               <div id="instructions" className="secondary_text">
-                <div id = "language">
-                  <h1>
-                    {this.state.language}
-                  </h1>
-                </div>
                 '⌘ /'	to show answer, '⌘ enter'	to advance to next card
               </div>
               <div id="qaWrapper">
@@ -96,7 +149,7 @@ class App extends Component {
                 {this.state.currentQuestion}
                 </div>
                 <form className="distributeEvenly">
-                  <textarea rows="10" cols="50">
+                  <textarea rows="10" cols="50" className="textarea">
                   </textarea>
                 </form>
                 <div id="answer" className="hidden distributeEvenly">
