a11d95858a05aace3b729d42731471d613a3f65d
Ross <Ross@myyearofcode.com>
2019-02-16 22:59:47 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/launchpad/precache-manifest.8543f9ee855734f8a400a78a25d8c29a.js"
+  "/launchpad/precache-manifest.05b9a2ed359c13944d693e1ce68dd010.js"
 );
 
 workbox.clientsClaim();
